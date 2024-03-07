589d0c9ccd9d24201bc555975416317d32d56ed1
Joël van Geest <joel.van.geest@student.nhlstenden.com>
2024-03-07 17:26:09 +0100

    Alle data wordt ingeladen van names en titles, multithread moet nog toegevoegd worden.

--- app/Python/importMovies.py
+++ app/Python/importMovies.py
@@ -17,33 +17,31 @@ class URLs(Enum):
     TITLE_PRINCIPALS = BASE_URL + "title.principals.tsv.gz"
     TITLE_RATINGS = BASE_URL + "title.ratings.tsv.gz"
 
-def stream_gzip_content(url, nr_of_rows):
+def stream_gzip_content(url):
     with requests.get(url, stream=True) as response:
         if response.status_code == 200:
             decompressor = zlib.decompressobj(zlib.MAX_WBITS | 16)
-            line_count = 0
             buffer = ""  # Buffer om gedeeltelijke rijen op te slaan
             for chunk in response.iter_content(chunk_size=1024):
-                decompressed_chunk = decompressor.decompress(chunk).decode('utf-8')
-                lines = (buffer + decompressed_chunk).split('\n')
+                decompressed_chunk = decompressor.decompress(chunk)
+                try:
+                    decoded_chunk = decompressed_chunk.decode('utf-8')
+                except UnicodeDecodeError:
+                    decoded_chunk = decompressed_chunk.decode('latin-1', errors='ignore')
+                lines = (buffer + decoded_chunk).split('\n')
                 buffer = lines.pop()  # Laatste element is mogelijk een gedeeltelijke rij
                 for line in lines:
                     yield line
-                    line_count += 1
-                    if line_count == nr_of_rows:
-                        return
         else:
             print("Er is een fout opgetreden bij het downloaden van het bestand.")
 
 
-def load_titles(conn, nr_of_rows, file = None):
+def load_titles(conn):
     """
-    Load and process TSV data from either a stream or a local file.
+    Load and process TSV data from  a stream.
 
     Args:
     conn (psycopg2.extensions.connection): A connection to the database.
-    nr_of_rows (int): Number of rows to process.
-    file(string): File to read data from. If None, data will be streamed.
 
     Returns:
     None
@@ -53,21 +51,13 @@ def load_titles(conn, nr_of_rows, file = None):
     COLUMN_NAMES = None
 
     # set data source
-    if file is None:
-        url = URLs.TITLE_BASICS.value
-        data_source = stream_gzip_content(url, nr_of_rows)
-    else:
-        # Read from local file
-        with open(file, 'r', encoding='utf-8') as f:
-            data_source = f.readlines()[:nr_of_rows]
+    url = URLs.TITLE_BASICS.value
+    data_source = stream_gzip_content(url)
 
     try:
         rows_added = 0
 
         for rows_processed, line in enumerate(data_source):
-            if rows_processed >= nr_of_rows:
-                break
-
             row = line.rstrip('\n').split('\t')  # Split de regel in velden
 
             # If it's the first row, extract column names
@@ -82,7 +72,6 @@ def load_titles(conn, nr_of_rows, file = None):
                     genre_id = create_and_get_genre_id(genre_name, conn)
                     genres_with_ids[genre_name] = genre_id
 
-
             with conn.cursor() as cursor:
                 cursor.execute("""
                     INSERT INTO titles (imdb_externid, primary_title, type, is_adult, start_year, end_year, runtime_minutes, original_title)
@@ -91,6 +80,7 @@ def load_titles(conn, nr_of_rows, file = None):
                     RETURNING id;
                 """, (row['tconst'], row['primaryTitle'], row['titleType'], row['isAdult'] if row['isAdult'] != '\\N' else None, row['startYear'] if row['startYear'] != '\\N' else None, row['endYear'] if row['endYear'] != '\\N' else None, row['runtimeMinutes'] if row['runtimeMinutes'] != '\\N' else None, row['originalTitle']))
 
+                print("Loaded " + row['primaryTitle'])
                 result = cursor.fetchone()
                 if result is not None:
                     rows_added += 1
@@ -106,7 +96,6 @@ def load_titles(conn, nr_of_rows, file = None):
                         VALUES (%s, %s);
                     """, (title_id, genre_id))
 
-
     except psycopg2.Error as e:
         conn.rollback()
         print("An error occurred during data processing:", e)
@@ -116,10 +105,9 @@ def load_titles(conn, nr_of_rows, file = None):
     print("Data bevat: " + str(len(genres_with_ids)) + " genres.")
     print(str(rows_added) + " nieuwe rijen toegevoegd.")
     print(str(rows_processed) + " rijen totaal in database")
-    method =  "stream" if file is None else "bestand"
     end_time = datetime.now()
     duration = end_time - start_time
-    print("Data ingeladen via " + method + " in " + str(duration))
+    print("Data ingeladen via stream in" + str(duration))
 
 def create_and_get_genre_id(genre_name, conn):
     """
@@ -146,7 +134,113 @@ def create_and_get_genre_id(genre_name, conn):
             cursor.execute("SELECT id FROM genres WHERE name = %s;", (genre_name,))
             return cursor.fetchone()[0]
 
+def load_name_basics(conn):
+    """
+    Load and process TSV data from a stream.
 
+    Args:
+    conn (psycopg2.extensions.connection): A connection to the database.
+
+    Returns:
+    None
+    """
+    start_time = datetime.now()
+    known_for_titles = {}
+    professions = {}
+    COLUMN_NAMES = None
+
+    # set data source
+    url = URLs.NAME_BASICS.value
+    data_source = stream_gzip_content(url)
+
+    try:
+        rows_added = 0
+
+        for rows_processed, line in enumerate(data_source):
+
+            row = line.rstrip('\n').split('\t')  # Split de regel in velden
+
+            # If it's the first row, extract column names
+            if rows_processed == 0:
+                COLUMN_NAMES = row
+                continue  # Skip processing the first row
+
+            row = dict(zip(COLUMN_NAMES, row))
+
+            for profession in row['primaryProfession'].split(','):
+                if profession not in professions:
+                    profession_id = create_and_get_profession_id(profession, conn)
+                    professions[profession] = profession_id
+
+            for title_id in row['knownForTitles'].split(','):
+                with conn.cursor() as cursor:
+                    # Query om de people_id op te halen aan de hand van de title_id
+                    cursor.execute("SELECT imdb_externid FROM titles WHERE imdb_externid = %s", (title_id,))
+                    result = cursor.fetchone()
+                    if result:
+                        db_title_id = result[0]
+                        cursor.execute("""
+                        INSERT INTO model_has_crew (model_id, people_id, person_is_known_for_model)
+                        VALUES (%s, %s, %s);
+                        """, (db_title_id, row['people_id'], TRUE))
+                    else:
+                        print("title_id is nog niet bekend: ", title_id)
+
+                    cursor.execute("""
+                        INSERT INTO people (imdb_externid, name, birth_year, death_year)
+                        VALUES (%s, %s, %s, %s)
+                        ON CONFLICT (imdb_externid) DO NOTHING
+                        RETURNING id;
+                    """, (row['nconst'], row['primaryName'], row['birthYear'] if row['birthYear'] != '\\N' else None, row['deathYear'] if row['deathYear'] != '\\N' else None))
+
+                    result = cursor.fetchone()
+                    if result is not None:
+                        rows_added += 1
+
+                    # Check if title is already imported
+                    if result is None:
+                        continue
+
+                    people_id = result[0]
+                    for profession, profession_id in professions.items():
+                        with conn.cursor() as cursor:
+                            cursor.execute("""
+                                INSERT INTO people_professions (people_id, profession_id)
+                                VALUES (%s, %s);
+                            """, (people_id, profession_id))
+
+
+    except psycopg2.Error as e:
+        conn.rollback()
+        print("An error occurred during data processing:", e)
+    else:
+        conn.commit()
+
+    print(str(rows_added) + " nieuwe rijen toegevoegd.")
+    print(str(rows_processed) + " rijen totaal in database")
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print("Data ingalden via stream in " +  str(duration))
+
+def create_and_get_profession_id(profession, conn):
+    with conn.cursor() as cursor:
+        cursor.execute("""
+           INSERT INTO professions (name)
+           VALUES (%s)
+           RETURNING id;
+        """, (profession,))
+        profession_id = cursor.fetchone()
+        if profession_id:
+            return profession_id[0]
+        else:
+            cursor.execute("SELECT id FROM professions WHERE name = %s;", (profession,))
+            return cursor.fetchone()[0]
+
+def get_total_rows(data_source):
+    total_rows = 0
+    for line in data_source:
+        total_rows += 1
+    return total_rows
 
 def main():
 
@@ -154,12 +248,13 @@ def main():
     relative_path_to_movie_data = 'storage/app/public/title.basics.tsv/title.basics.tsv'
     movie_data_path = os.path.join(project_folder, relative_path_to_movie_data)
 
-    nr_of_rows = 500
-
     connection = db.get_connection()
 
     # Load data via stream
-    load_titles(connection, nr_of_rows)
+    load_titles(connection)
+    load_name_basics(connection)
+
+
 
     # Load data via bestand (let op: 3x langzamer!)
     # load_titles(connection, nr_of_rows, movie_data_path)
