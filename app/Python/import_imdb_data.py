f578f15c354c1cdf08851f11d595cac5f292f6c0
biebthesecond <jeltehoekstramakkum@gmail.com>
2024-03-14 17:27:08 +0100

    import ratings

--- app/Python/import_imdb_data.py
+++ app/Python/import_imdb_data.py
@@ -2,12 +2,16 @@ import import_scripts.import_titles as import_titles
 import import_scripts.import_name_basics as import_names
 import import_scripts.import_alternate_titles as import_alternate_titles
 import import_scripts.import_title_episodes as import_episodes
+import import_scripts.import_crew as import_crew
+import import_scripts.import_ratings as import_ratings
 
 def main():
     import_titles.execute()
-    import_names.execute()
     import_alternate_titles.execute()
     import_episodes.execute()
+    import_names.execute()
+    import_crew.execute()
+    import_ratings.execute()
 
 
 if __name__ == "__main__":
