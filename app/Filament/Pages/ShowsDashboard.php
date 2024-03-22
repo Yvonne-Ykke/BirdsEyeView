4f875a7efb439202c2a7cee873aca543c4c8b738
Yvonne-Ykke <yvonne.koeroo@student.nhlstenden.com>
2024-03-22 13:24:15 +0100

    wip shows dashboard

---
+++ app/Filament/Pages/ShowsDashboard.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Widgets\Shows\ShowRatingsGenreChart;
+use Illuminate\Support\Facades\Route;
+
+
+class ShowsDashboard extends \Filament\Pages\Dashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-video-camera';
+    protected static string $routePath = 'shows-dashboard';
+    protected static ?int $navigationSort = 0;
+
+    protected static ?string $navigationLabel = 'Series';
+
+    protected static ?string $title = 'Series dashboard';
+
+
+    public static function getNavigationLabel(): string
+    {
+        return static::$navigationLabel ?? static::$title ?? __('filament::pages/dashboard.title');
+    }
+
+    public static function getRoutes(): \Closure
+    {
+        return function () {
+            Route::get('/shows-dashboard', static::class)->name(static::getSlug());
+        };
+    }
+
+    public function getWidgets(): array
+    {
+        return [
+            // ShowRatingsGenreChart::class,
+        ];
+    }
+
+    public function getColumns(): int | string | array
+    {
+        return 2;
+    }
+
+    public function getTitle(): string
+    {
+        return static::$title ?? __('filament::pages/dashboard.title');
+    }
+}
