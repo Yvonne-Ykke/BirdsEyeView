3670ac1abd133d1e38e5405493c798f1c885e003
biebthesecond <jeltehoekstramakkum@gmail.com>
2024-03-25 12:40:52 +0100

    colors enum

---
+++ app/Support/Colors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Support;
+
+enum Colors: string
+{
+    case RED = '#dc2626';
+    case NEUTRAL = '#525252';
+    case ORANGE = '#ea580c';
+    case AMBER = '#d97706';
+    case YELLOW = '#ca8a04';
+    case LIME = '#65a30d';
+    case GREEN = '#16a34a';
+    case TEAL = '#0d9488';
+    case INDIGO = '#4f46e5';
+    case VIOLET = '#7c3aed';
+    case FUCHSIA = '#c026d3';
+    case PINK = '#db2777';
+    case ROSE = '#e11d48';
+}
