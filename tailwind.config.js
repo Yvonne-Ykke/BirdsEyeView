b9a3489e7d3ed9fb4f2897d5265e01fd8ebe1cda
biebthesecond <jeltehoekstramakkum@gmail.com>
2024-04-05 12:39:50 +0200

    custom theme to use tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+
 export default {
   content: [
       './resources/**/*.blade.php',
       './vendor/filament/**/*.blade.php',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            primary: colors.amber,
+            danger: colors.red,
+            success: colors.green,
+            warning: colors.amber,
+            transparent: 'transparent',
+            gray: colors.gray,
+            red: colors.red,
+            orange: colors.orange,
+            yellow: colors.amber,
+            lime: colors.lime,
+            green: colors.emerald,
+            cyan: colors.cyan,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.purple,
+            pink: colors.pink,
+            black: colors.black,
+            white: colors.white,
+        },
+    },
   },
-  plugins: [],
+    plugins: [
+        forms,
+        typography,
+    ],
 }
 
